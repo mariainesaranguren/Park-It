24c3fef0e4f4a594d1e9ab347bde52199a732b4f
Maria Ines Aranguren <marangu1@nd.edu>
2018-04-08 14:02:14 -0400

    First commit

---
+++ app/src/main/java/com/example/mariainesaranguren/parkit/MapsActivity.kt
@@ -0,0 +1,145 @@
+package com.example.mariainesaranguren.parkit
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import android.location.Location
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.parse.Parse
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    companion object {
+        private const val findParkingZoom: Float = 15.0f
+        private val notreDamePosition: LatLng = LatLng(41.7056, -86.2353)
+        private const val MY_PERMISSIONS_REQUEST_LOCATION: Int = 1
+    }
+    // Zoom levels
+    //    * 10: City
+    //    * 15: Streets
+    //    * 20: Buildings
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Parse.initialize(this)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        // Get last known location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        showLocation()
+    }
+
+    fun showLocation(){
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("showLocation", "Doesn't have permission")
+            Log.d("showLocation", "Going to request permission")
+
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION)
+
+        } else {
+            // Permission has already been granted
+            var position: LatLng = notreDamePosition
+            Log.d("showLocation", "Position set to ND")
+
+            fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            position = LatLng(location.getLatitude(), location.getLongitude())
+                            Log.d("showLocation", "Position set to current location")
+                            Log.v("showLocation", position.toString())
+                            setCamera(position, findParkingZoom)
+                        }
+                    }
+                    .addOnFailureListener {
+                        val toast = Toast.makeText(this, "Unable to get current location", Toast.LENGTH_SHORT)
+                        toast.show()
+                    }
+        }
+    }
+
+    fun setCamera(location: LatLng, zoom: Float) {
+        mMap.addMarker(MarkerOptions().position(location).title("Notre Dame"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, zoom))
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                var position: LatLng = notreDamePosition
+                Log.d("onRequestPer", "Position set to ND")
+                Log.d("onRequestPer", "Permission granted")
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission granted
+                    // Get most recent location and set camera
+                    fusedLocationClient.lastLocation
+                            .addOnSuccessListener { location: Location? ->
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    position = LatLng(location.getLatitude(), location.getLongitude())
+                                    Log.v("onRequestPer", position.toString())
+                                    setCamera(position, findParkingZoom)
+                                }
+                                Log.d("onRequestPer", "Position set to current location")
+                            }
+                            .addOnFailureListener {
+                                val toast = Toast.makeText(this, "Unable to get current location", Toast.LENGTH_SHORT)
+                                toast.show()
+                            }
+
+                } else {
+                    // Permission denied
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+}
+
